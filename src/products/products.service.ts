29d80b769dd65a6b254b360a19df4522f145f94a
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-21 20:05:53 +0100

    removed unused import

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Product } from '@prisma/client';
+
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
