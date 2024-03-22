634f1874d89d3b6d4f6f13228bb44fa7e7b55acb
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-22 17:02:43 +0100

    ProductEntity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,30 @@
-export class Product {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Product } from '@prisma/client';
+export class ProductEntity implements Product {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  image: string;
+
+  @ApiProperty()
+  price: number;
+
+  @ApiProperty()
+  ratingId: number;
+
+  @ApiProperty()
+  storeId: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
