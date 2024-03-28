683a7967f7ee184c3e7eac470617436bfbc08bc8
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-28 13:16:54 +0100

    cart-items auth guards

--- Backend/src/carts/cart-items.controller.ts
+++ Backend/src/carts/cart-items.controller.ts
@@ -1,27 +1,41 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { CartItemsService } from './cart-items.service';
 import { CreateCartItemDto } from './dto/create-cart-item.dto';
 import { ApiTags, ApiOkResponse, ApiCreatedResponse } from '@nestjs/swagger';
 import { CartItemEntity } from './entities/cart.entity';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { UserAuthGuard } from 'src/users/guards/user-auth.guard';
 
 @Controller('cart-items')
 @ApiTags('cart items')
+@ApiBearerAuth()
 export class CartItemsController {
   constructor(private readonly cartItemsService: CartItemsService) {}
 
   @Post()
+  @UseGuards(UserAuthGuard)
   @ApiCreatedResponse({ type: CartItemEntity })
   create(@Body() createCartDto: CreateCartItemDto) {
     return this.cartItemsService.create(createCartDto);
   }
 
   @Get(':userId')
+  @UseGuards(UserAuthGuard)
   @ApiOkResponse({ type: CartItemEntity, isArray: true })
   findByUserId(@Param('userId') userId: string) {
     return this.cartItemsService.findByUserId(+userId);
   }
 
   @Delete(':id')
+  @UseGuards(UserAuthGuard)
   @ApiCreatedResponse({ type: CartItemEntity })
   remove(@Param('id') id: string) {
     return this.cartItemsService.remove(+id);
