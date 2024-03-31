ca2bca93d53b027a57a0252bbc9a7f0aa839c966
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-31 19:30:01 +0200

    added type attribute

--- Frontend/src/components/Inputs/TextInput/TextInput.jsx
+++ Frontend/src/components/Inputs/TextInput/TextInput.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./index.module.css";
 
-const TextInput = ({ value, setValue, placeholder }) => {
+const TextInput = ({ value, setValue, placeholder, type = "text" }) => {
   return (
     <input
       className={styles["input"]}
-      type="text"
+      type={type}
       value={value}
       onChange={(e) => setValue(e.target.value)}
       placeholder={placeholder}
