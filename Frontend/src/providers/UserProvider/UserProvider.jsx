cc47a2ee8e474c451423a616c3d49805e9ce363b
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-02 22:20:04 +0200

    user provider

---
+++ Frontend/src/providers/UserProvider/UserProvider.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState } from "react";
+
+const Context = createContext({
+  user: null,
+  setUser: () => {},
+  isLoggedIn: false,
+  setIsLoggedIn: () => {},
+});
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  return (
+    <Context.Provider value={{ user, setUser, isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useUser = () => {
+  return useContext(Context);
+};
+
+export { UserProvider };
