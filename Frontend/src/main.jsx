c8a31e6feed8843126ef8dcd8a1f421df090171d
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-02 22:20:57 +0200

    mounting providers

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import UserProvider from "./providers/UserProvider";
+import CartProvider from "./providers/CartProvider";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Toaster />
-      <App />
+      <UserProvider>
+        <CartProvider>
+          <Toaster />
+          <App />
+        </CartProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
