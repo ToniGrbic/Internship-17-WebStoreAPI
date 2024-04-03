420c106bbca18f218240156472abeb0b6835cbc7
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-03 18:39:49 +0200

    isloggedin default value false

--- Frontend/src/providers/UserProvider/UserProvider.jsx
+++ Frontend/src/providers/UserProvider/UserProvider.jsx
@@ -9,7 +9,7 @@ const Context = createContext({
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <Context.Provider value={{ user, setUser, isLoggedIn, setIsLoggedIn }}>
