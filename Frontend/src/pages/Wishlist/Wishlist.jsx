6dcad4fe698a818504266e70d0e7097b47268199
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-03 23:51:26 +0200

    wishlist page

--- Frontend/src/pages/Wishlist/Wishlist.jsx
+++ Frontend/src/pages/Wishlist/Wishlist.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import styles from "./index.module.css";
+import useFetchProducts from "../../hooks/useFetchProducts";
+import { useNavigate } from "react-router-dom";
 
 const Wishlist = () => {
-  return <div>Wishlist</div>;
+  const { products } = useFetchProducts();
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles["wishlist-container"]}>
+      <h1>Wishlist</h1>
+      {products.map((product) => {
+        return (
+          <div
+            className={styles["wishlist-item-container"]}
+            onClick={() =>
+              navigate(`/product/${product.id}`, { state: product })
+            }
+            key={product.id}
+          >
+            <div className={styles["wishlist-item"]}>
+              <img
+                src={product.image}
+                alt="product"
+                className={styles["wishlist-img"]}
+              />
+              <div className={styles["wishlist-details"]}>
+                <h3>{product.title}</h3>
+                <p>${product.price}</p>
+              </div>
+              <div className={styles["wishlist-btn-container"]}>
+                <button className={styles["wishlist-btn"]}>Remove</button>
+                <button className={styles["wishlist-btn"]}>Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Wishlist;
