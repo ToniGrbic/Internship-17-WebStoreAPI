597f89c65ae721bfb7a8d23b1df368f1501bf634
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-03 23:51:36 +0200

    orders page

--- Frontend/src/pages/Orders/Orders.jsx
+++ Frontend/src/pages/Orders/Orders.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import styles from "./index.module.css";
+import useFetchProducts from "../../hooks/useFetchProducts";
+import { useNavigate } from "react-router-dom";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { products } = useFetchProducts();
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles["orders-container"]}>
+      <h1>Orders</h1>
+      {products.map((product) => {
+        return (
+          <div
+            className={styles["orders-item-container"]}
+            onClick={() =>
+              navigate(`/product/${product.id}`, { state: product })
+            }
+            key={product.id}
+          >
+            <div className={styles["orders-item"]}>
+              <img
+                src={product.image}
+                alt="product"
+                className={styles["orders-img"]}
+              />
+              <div className={styles["orders-details"]}>
+                <h3>{product.title}</h3>
+                <p>${product.price}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Orders;
