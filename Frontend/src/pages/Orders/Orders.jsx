0c5546b7f6be5f35f9d4207fbaf7b775d4bcd3ec
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-05 22:22:03 +0200

    orders rendering

--- Frontend/src/pages/Orders/Orders.jsx
+++ Frontend/src/pages/Orders/Orders.jsx
@@ -1,38 +1,46 @@
 import React from "react";
 import styles from "./index.module.css";
-import useFetchProducts from "../../hooks/useFetchProducts";
+import { useUser } from "../../providers/UserProvider/UserProvider";
 import { useNavigate } from "react-router-dom";
 
 const Orders = () => {
-  const { products } = useFetchProducts();
+  const { orders } = useUser();
   const navigate = useNavigate();
 
   return (
     <div className={styles["orders-container"]}>
       <h1>Orders</h1>
-      {products.map((product) => {
-        return (
-          <div
-            className={styles["orders-item-container"]}
-            onClick={() =>
-              navigate(`/product/${product.id}`, { state: product })
-            }
-            key={product.id}
-          >
-            <div className={styles["orders-item"]}>
-              <img
-                src={product.image}
-                alt="product"
-                className={styles["orders-img"]}
-              />
-              <div className={styles["orders-details"]}>
-                <h3>{product.title}</h3>
-                <p>${product.price}</p>
+      {orders.length < 1 ? (
+        <h2>No orders yet...</h2>
+      ) : (
+        orders.map((product) => {
+          return (
+            <div
+              className={styles["orders-item-container"]}
+              onClick={() =>
+                navigate(`/product/${product.id}`, { state: product })
+              }
+              key={product.id}
+            >
+              <div className={styles["orders-item"]}>
+                <img
+                  src={product.image}
+                  alt="product"
+                  className={styles["orders-img"]}
+                />
+                <div className={styles["orders-details"]}>
+                  <h3>{product.title}</h3>
+                  <div className={styles["orders-sub-details"]}>
+                    <p> Total: ${product.price}</p>
+                    <p> Quantity: {product.quantity}</p>
+                    <p>Status: {product.status}</p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };
