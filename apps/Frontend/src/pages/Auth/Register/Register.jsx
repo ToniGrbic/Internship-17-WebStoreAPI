30e60c23a4947652563da0f675708fd889c07769
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-10 20:06:16 +0200

    register post req

--- apps/Frontend/src/pages/Auth/Register/Register.jsx
+++ apps/Frontend/src/pages/Auth/Register/Register.jsx
@@ -6,13 +6,14 @@ import toast from "react-hot-toast";
 
 const Register = () => {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!email || !password || !confirmPassword) {
+    if (!email || !password || !confirmPassword || !name) {
       toast.error("Please fill all fields!");
       return;
     }
@@ -21,17 +22,32 @@ const Register = () => {
       return;
     }
 
+    try {
+      const res = await fetch("http://localhost:3000/api/users/register", {
+        method: "POST",
+        body: JSON.stringify({ name, email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (!res.ok) {
+        throw new Error("something went wrong, try again later!");
+      }
+
+      toast.success("Account created successfully!");
+      navigate("/login");
+    } catch (error) {
+      toast.error("something went wrong, try again later!");
+    }
     setEmail("");
     setPassword("");
     setConfirmPassword("");
-    toast.success("Account created successfully!");
-    navigate("/login");
   };
   return (
     <>
       <h1>Sign up</h1>
       <form className={styles["login-form"]} onSubmit={handleSubmit}>
         <TextInput placeholder="Email" value={email} setValue={setEmail} />
+        <TextInput placeholder="Username" value={name} setValue={setName} />
         <TextInput
           placeholder="Password"
           value={password}
