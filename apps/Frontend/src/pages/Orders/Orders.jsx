e90093cb393d1573805726a68f461c84db290010
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-10 23:28:58 +0200

    fetch orders

--- apps/Frontend/src/pages/Orders/Orders.jsx
+++ apps/Frontend/src/pages/Orders/Orders.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./index.module.css";
 import { useUser } from "../../providers/UserProvider/UserProvider";
 import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+import toast from "react-hot-toast";
+import { baseUrl } from "../../constants/constants";
 
 const Orders = () => {
-  const { orders } = useUser();
+  const { setOrders, orders } = useUser();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const cookies = new Cookies();
+      const token = cookies.get("token");
+      if (!token) {
+        navigate("/");
+        return;
+      }
+
+      try {
+        const res = await fetch(`${baseUrl}/orders`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        if (!res.ok) {
+          throw new Error("Something went wrong, try again later!");
+        }
+
+        const orders = await res.json();
+        setOrders(orders);
+      } catch (error) {
+        toast.error("Something went wrong, try again later!");
+      }
+    })();
+  }, []);
 
   return (
     <div className={styles["orders-container"]}>
@@ -13,27 +42,29 @@ const Orders = () => {
       {orders.length < 1 ? (
         <h2>No orders yet...</h2>
       ) : (
-        orders.map((product) => {
+        orders.map((order) => {
           return (
             <div
               className={styles["orders-item-container"]}
               onClick={() =>
-                navigate(`/product/${product.id}`, { state: product })
+                navigate(`/product/${order.product.id}`, {
+                  state: order.product,
+                })
               }
-              key={product.id}
+              key={order.product.id}
             >
               <div className={styles["orders-item"]}>
                 <img
-                  src={product.image}
+                  src={order.product.image}
                   alt="product"
                   className={styles["orders-img"]}
                 />
                 <div className={styles["orders-details"]}>
-                  <h3>{product.title}</h3>
+                  <h3>{order.product.title}</h3>
                   <div className={styles["orders-sub-details"]}>
-                    <p> Total: ${product.price}</p>
-                    <p> Quantity: {product.quantity}</p>
-                    <p>Status: {product.status}</p>
+                    <p> Total: ${order.product.price}</p>
+                    <p> Quantity: {order.quantity}</p>
+                    <p>Status: {order.status}</p>
                   </div>
                 </div>
               </div>
