06d9379e9588db4f2ba665a158b038bb1f4e32a7
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-13 01:05:43 +0200

    removed post to cart-items on logout

--- apps/Frontend/src/components/Dropdown/Dropdown.jsx
+++ apps/Frontend/src/components/Dropdown/Dropdown.jsx
@@ -1,42 +1,17 @@
 import React from "react";
 import styles from "./index.module.css";
 import { toast } from "react-hot-toast";
-import { useCartContext } from "../../providers/CartProvider/CartProvider";
 import { useUser } from "../../providers/UserProvider/UserProvider";
 import { useNavigate } from "react-router-dom";
 import Cookies from "universal-cookie";
-import { baseUrl } from "../../constants/constants";
 
 const Dropdown = ({ showDropdown }) => {
   const { setUser } = useUser();
-  const { cartItems, onRemoveAll } = useCartContext();
   const navigate = useNavigate();
 
   const cookies = new Cookies(null, { path: "/" });
-  const token = cookies.get("token");
 
   const handleLogout = async () => {
-    const cartItemsPromises = cartItems.map((item) => {
-      return fetch(`${baseUrl}/cart-items`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          productId: item.product.id,
-          quantity: item.quantity,
-        }),
-      });
-    });
-
-    try {
-      await Promise.all(cartItemsPromises);
-      onRemoveAll();
-    } catch (error) {
-      toast.error("Something went wrong, try again later!");
-    }
-
     setUser(null);
     navigate("/");
     cookies.remove("token");
