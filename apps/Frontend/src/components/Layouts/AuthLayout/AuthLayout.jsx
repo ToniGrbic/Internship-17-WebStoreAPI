b824d36dca70d083b29ce40cb1f30a12c7556a12
ToniGrbic <tonigrbic.5@gmail.com>
2024-04-21 01:03:37 +0200

    added home link to AuthLayout

--- apps/Frontend/src/components/Layouts/AuthLayout/AuthLayout.jsx
+++ apps/Frontend/src/components/Layouts/AuthLayout/AuthLayout.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import styles from "./index.module.css";
 import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa6";
 
 const AuthLayout = () => {
   return (
     <div className={styles["auth-wrapper"]}>
       <div className={styles["auth-container"]}>
+        <div className={styles["auth-home-link"]}>
+          <Link to="/">
+            <FaArrowLeft />
+            Home
+          </Link>
+        </div>
+
         <Outlet />
       </div>
     </div>
